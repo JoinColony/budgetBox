5a23fcbf0c7bad4e3750da1fa48dbf6925f2303f
Daniel Kronovet <kronovet@gmail.com>
2018-09-08 13:15:52 +0200

    Construct budget box from vote array

--- contracts/BudgetBox.sol
+++ contracts/BudgetBox.sol
@@ -20,14 +20,54 @@ pragma experimental "v0.5.0";
 
 
 contract BudgetBox {
-  uint256 k;
-  bytes32[] voteArray;
+  uint256 constant K = 10;
+  uint256[] voteArray;
 
-  function addVote(bytes32 vote) public {
+  //////////
+  // Public functions
+
+  function addVote(uint256 vote) public {
     voteArray.push(vote);
   }
 
   function numVotes() public view returns (uint256) {
     return voteArray.length;
+  }
+
+  function createBBox() public view returns (uint256[K][K]) {
+    uint256[K][K] memory bbox;
+    uint a;
+    uint b;
+    uint pos;
+
+    for (uint voter; voter < voteArray.length; voter++) {
+      for (b = 0; b < K; b++) {
+        for (a = 0; a < b; a++) {
+          pos = getPos(a, b);
+          if (voteExists(voter, pos)) {
+            // Note: true indicates that b is preferred
+            getVote(voter, pos) ? bbox[b][a] += 1 : bbox[a][b] += 1;
+          }
+        }
+      }
+    }
+
+    return bbox;
+  }
+
+  //////////
+  // Internal functions
+
+  function voteExists(uint vote, uint pos) internal view returns (bool) {
+    return voteArray[vote] & (2 << pos * 2) > 0;
+  }
+
+  function getVote(uint vote, uint pos) internal view returns (bool) {
+    return voteArray[vote] & (1 << pos * 2) > 0;
+  }
+
+  function getPos(uint a, uint b) internal pure returns (uint) {
+    require(a < b, "wrong-argument-order");
+    return ((b * (b - 1)) / 2) + a;
   }
 }
--- test/budgetbox.js
+++ test/budgetbox.js
@@ -1,5 +1,11 @@
 var BudgetBox = artifacts.require("./BudgetBox.sol");
 
+function createVote(winner, loser) {
+    const [a, b, winbit] = (winner < loser) ? [winner, loser, 0] : [loser, winner, 1];
+    const pos = ((b * (b - 1)) / 2) + a;
+    return (2 + winbit) << (pos * 2);
+}
+
 contract('BudgetBox', function(accounts) {
   let budgetBox;
 
@@ -8,12 +14,27 @@ contract('BudgetBox', function(accounts) {
   })
 
   it("should add a series of votes", async () => {
-    await budgetBox.addVote(0x01);
-    await budgetBox.addVote(0x02);
-    await budgetBox.addVote(0x03);
+    await budgetBox.addVote(0);
+    await budgetBox.addVote(0);
+    await budgetBox.addVote(0);
 
     const numVotes = await budgetBox.numVotes();
     assert.equal(numVotes.toNumber(), 3);
+  });
+
+  it("should generate a bbox", async () => {
+    await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
+
+    const bbox = await budgetBox.createBBox();
+    assert.equal(bbox[0][1].toNumber(), 1);
+    assert.equal(bbox[1][2].toNumber(), 1);
+
+    assert.equal(bbox[1][0].toNumber(), 0);
+    assert.equal(bbox[2][1].toNumber(), 0);
+
+    assert.equal(bbox[0][0].toNumber(), 0);
+    assert.equal(bbox[1][1].toNumber(), 0);
+    assert.equal(bbox[2][2].toNumber(), 0);
   });
 
 });
