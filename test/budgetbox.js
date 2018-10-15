7a4e39a6aa404c1b0b701abfacc8bfcdcc125cca
Daniel Kronovet <kronovet@gmail.com>
2018-10-15 18:08:22 +0300

    Add more gas tests

--- test/budgetbox.js
+++ test/budgetbox.js
@@ -1,5 +1,6 @@
 const sumMatrix = require("./utilities").sumMatrix;
 const createVote = require("./utilities").createVote;
+const createRandomVotes = require("./utilities").createRandomVotes;
 
 const BudgetBox = artifacts.require("./BudgetBox.sol");
 
@@ -9,42 +10,61 @@ contract('BudgetBox', function(accounts) {
 
   beforeEach(async () => {
     budgetBox = await BudgetBox.new();
-  })
-
-  it("should add a series of votes", async () => {
-    await budgetBox.addVote(0);
-    await budgetBox.addVote(0);
-    await budgetBox.addVote(0);
-
-    const numVotes = await budgetBox.numVotes();
-    assert.equal(numVotes.toNumber(), 3);
   });
 
-  it("should generate a bbox", async () => {
-    await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
+  describe("general functionality", async () => {
+    it("should add a series of votes", async () => {
+      await budgetBox.addVote(0);
+      await budgetBox.addVote(0);
+      await budgetBox.addVote(0);
 
-    const bbox = await budgetBox.createBBox();
-    assert.equal(bbox[0][1].toNumber(), 1);
-    assert.equal(bbox[1][2].toNumber(), 1);
-    assert.equal(sumMatrix(bbox), 2);
+      const numVotes = await budgetBox.numVotes();
+      assert.equal(numVotes.toNumber(), 3);
+    });
+
+    it("should generate a bbox", async () => {
+      await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
+
+      const bbox = await budgetBox.createBBox();
+      assert.equal(bbox[0][1].toNumber(), 1);
+      assert.equal(bbox[1][2].toNumber(), 1);
+      assert.equal(sumMatrix(bbox), 2);
+    });
+
+    it("should create the markov matrix and run the power method", async () => {
+      await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
+
+      let bbox;
+      bbox = await budgetBox.createBBox();
+      bbox = await budgetBox.addDiagonal(bbox);
+      bbox = await budgetBox.normalize(bbox);
+
+      const v = await budgetBox.powerMethod(bbox);
+      assert(v[0].toNumber() > v[1].toNumber());
+      assert(v[1].toNumber() > v[2].toNumber());
+    });
+
+    it("should run everything at once", async () => {
+      await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
+      await budgetBox.runAll();
+    });
   });
 
-  it("should run the power method", async () => {
-    await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
+  describe("gas costs for various degrees of scale", async () => {
+    [10, 50, 100, 150, 200].forEach(async (n) => {
+      it(`should load ${n} votes`, async () => {
+        for (var i = 0; i < n; i++) {
+          await budgetBox.addVote(createRandomVotes().toString());
+        }
+      });
 
-    let bbox;
-    bbox = await budgetBox.createBBox();
-    bbox = await budgetBox.addDiagonal(bbox);
-    bbox = await budgetBox.normalize(bbox);
-
-    const v = await budgetBox.powerMethod(bbox);
-    assert(v[0].toNumber() > v[1].toNumber());
-    assert(v[1].toNumber() > v[2].toNumber());
-  });
-
-  it("should run everything", async () => {
-    await budgetBox.addVote(createVote(0, 1) | createVote(1, 2));
-    await budgetBox.runAll();
+      it(`should load and process ${n} votes`, async () => {
+        for (var i = 0; i < n; i++) {
+          await budgetBox.addVote(createRandomVotes().toString());
+        }
+        await budgetBox.runAll();
+      });
+    });
   });
 
 });
--- test/utilities.js
+++ test/utilities.js
@@ -1,7 +1,26 @@
+const BN = require("bn.js");
+
+
 function createVote(winner, loser) {
     const [a, b, winbit] = (winner < loser) ? [winner, loser, 0] : [loser, winner, 1];
     const pos = ((b * (b - 1)) / 2) + a;
-    return (2 + winbit) << (pos * 2);
+    return (new BN(2 + winbit)).ushln(pos * 2);
+}
+
+// https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+var seed = 1;
+function random() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+}
+
+function createRandomVotes() {
+  const max16 = 2 ** 16 - 1
+  const maxVote = new BN(max16 * random());
+  for (var i = 0; i < 15; i++) {
+    maxVote.ishln(16).iaddn(max16 * random());
+  }
+  return maxVote;
 }
 
 function sumMatrix(matrix) {
@@ -20,6 +39,7 @@ function printRow(row) {
 
 module.exports = {
   createVote,
+  createRandomVotes,
   sumMatrix,
   printMatrix,
   printRow,
